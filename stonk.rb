4500eb34a79e13821093eba315e1cabcb2f48d28
codekane <ryan.horricks@gmail.com>
2021-04-19 23:34:53 -0600

    Integrate the yahooquery Python library. Get Stonk Data, Bitches!

--- stonk.rb
+++ stonk.rb
@@ -5,20 +5,26 @@ require './models/stonk.rb'
 require 'json'
 
 
+# Ruby method to act as a pass-through for the stonk_data.py script
+
+module YahooFinance
+
+  def self.get_stonk_data(string)
+    output = `python lib/stonk_data.py #{string}`
+    return JSON.parse(output)
+  end
+
+end
+
+
+
 class APIController < Sinatra::Base
 
   get '/api/stonks' do
     content_type 'application/json'
-
     return Stonk.all.pluck(:symbol).to_json
-    # Should respond 200
-    # Format should be JSON
-    # Response should match a query of Stock.all.symbol:=
-    # Should return the symbols for Stock.all in JSON format.
-
   end
 
   get 'stonk_data' do
-
   end
 end
